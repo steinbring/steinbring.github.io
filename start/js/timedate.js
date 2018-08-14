3cb3d0d12c4e22d9b24f57b1f0160832dba703a8
josephs7 <josephs7@uwm.edu>
2018-08-14 14:30:31 -0500

    I localized it a little

--- start/js/timedate.js
+++ start/js/timedate.js
@@ -1,7 +1,7 @@
 function timedate () {
   var d = new Date();
-  var days = ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag'];
-  var months = ['Jänner', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'];
+  var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
   document.getElementById('time').innerHTML = ('0' + d.getHours()).slice(-2) + ':' + ('0' + d.getMinutes()).slice(-2);
   document.getElementById('date').innerHTML = days[d.getDay()] + ', ' + d.getDate() + '. ' + months[d.getMonth()] + ' ' + d.getFullYear();
 }
